bc50328394437c43a679bf1ec0d331392b5b1da6
Redwan Mohammedberhan <red.01yassin@gmail.com>
2018-11-08 20:23:53 +0100

    frontpictureuploded
    need to tix the width

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Slider from "react-slick";
+import Art from "./art.jpg";
+import Basket from "./basket.jpg";
+import Urban from "./urban.jpg";
+import "./Image.css";
 class Image extends React.Component {
   constructor(props) {
     super(props);
@@ -7,7 +12,26 @@ class Image extends React.Component {
     };
   }
   render() {
-    return <div>{this.state.name}</div>;
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider className="Image" {...settings}>
+        <div>
+          <img src={Art} width="100%" height="500" />
+        </div>
+        <div>
+          <img src={Basket} width="100%" height="500" />
+        </div>
+        <div>
+          <img src={Urban} width="100%" height="500" />
+        </div>
+      </Slider>
+    );
   }
 }
 export default Image;
